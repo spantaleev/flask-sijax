275a0273bfb20ed0c468e5e3829c9ae9b875cd35
Slavi Pantaleev <s.pantaleev@gmail.com>
2015-01-15 11:52:09 +0900

    Release 0.4.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
     name = "Flask-Sijax",
-    version = '0.3.3',
+    version = '0.4.0',
     description = "An extension for the Flask microframework that adds Sijax support.",
     long_description = __doc__,
     author = "Slavi Pantaleev",
